3d8ad9bb45b5c171a325959c59edf60a57a69547
Grzegorz Rusztyn <grzebol112@gmail.com>
2024-06-01 21:27:38 +0200

    start
    main commit

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("com.example.renderinginfo", "Rendering Info Plugin", "1.0.0")]
+public class RenderingInfoPlugin : BaseUnityPlugin
+{
+    void Awake()
+    {
+        Logger.LogInfo("RenderingInfoPlugin is loaded and Awake is called");
+        LogRenderingAPI(); // Force call to LogRenderingAPI from Awake
+    }
+
+    void Start()
+    {
+        Logger.LogInfo("Start method is called");
+        // LogRenderingAPI(); // Optional: Comment this if called in Awake
+    }
+
+    void LogRenderingAPI()
+    {
+        Logger.LogInfo("LogRenderingAPI method is called");
+        string graphicsDeviceType = SystemInfo.graphicsDeviceType.ToString();
+        string graphicsDeviceName = SystemInfo.graphicsDeviceName;
+        string graphicsDeviceVersion = SystemInfo.graphicsDeviceVersion;
+
+        Logger.LogInfo($"Graphics Device Type: {graphicsDeviceType}");
+        Logger.LogInfo($"Graphics Device Name: {graphicsDeviceName}");
+        Logger.LogInfo($"Graphics Device Version: {graphicsDeviceVersion}");
+    }
+}
